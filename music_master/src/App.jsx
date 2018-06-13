0621eb723fcdecd8bcb78db2d7095536b25e72b1
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-06-13 11:54:01 +0100

    [react_redux_music-master] - search method

--- music_master/src/App.jsx
+++ music_master/src/App.jsx
@@ -2,27 +2,45 @@ import React from 'react';
 import Profile from './components/Profile.jsx';
 import Gallery from './components/Gallary.jsx';
 import './App.css';
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import {Glyphicon} from 'react-bootstrap';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			query: ''
+		}
+	}
+
+	setQuery() {
+		this.setState({
+			query: this.state.inputValue
+		})
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+	}
+
 	render() {
 		return (
 			<div className={'music-master'}>
 				<h1 className="music_master__title">Music Master</h1>
-				<FormGroup >
-					<InputGroup>
-						<FormControl
-							type="text"
-							placeholder='Search an artist'/>
-						<InputGroup.Addon>
-							<Glyphicon glyph="search"></Glyphicon>
-						</InputGroup.Addon>
-					</InputGroup>
-				</FormGroup>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						placeholder='Search an artist'
+						onChange={ event => this.setState({ inputValue: event.target.value }) }/>
+					<button onClick={ () => { this.setQuery() } }>
+						<Glyphicon glyph="search"></Glyphicon>
+					</button>
+				</form>
 				<div className="music_master__result">
-					<Profile />
-					<Gallery />
+					<Profile/>
+					<Gallery/>
 				</div>
+				<h1>{this.state.query}</h1>
 			</div>
 		)
 	}
