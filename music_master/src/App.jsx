41f2a9bcda3bb44e2fd8fb9d872d5adf29196da1
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-06-13 09:50:41 +0100

    [react_redux_music-master] - basic style setup

--- music_master/src/App.jsx
+++ music_master/src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Profile from './components/Profile.jsx';
 import Gallery from './components/Gallary.jsx';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className={'music-master'}>
 				<h1 className="music_master__title">Music Master</h1>
-				<form >
-					<input
-						type="text"
-						placeholder='Search an artist'/>
-					<button type='submit'>Search</button>
-				</form>
+				<FormGroup >
+					<InputGroup>
+						<FormControl
+							type="text"
+							placeholder='Search an artist'/>
+						<InputGroup.Addon>
+							<Glyphicon glyph="search"></Glyphicon>
+						</InputGroup.Addon>
+					</InputGroup>
+				</FormGroup>
 				<div className="music_master__result">
 					<Profile />
 					<Gallery />
