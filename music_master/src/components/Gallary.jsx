b251672a240121da84552fbe08ae0db5c95c188d
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-06-15 10:20:43 +0100

    [react_redux_music-master] - switch/pause music

--- music_master/src/components/Gallary.jsx
+++ music_master/src/components/Gallary.jsx
@@ -2,10 +2,41 @@ import React from 'react';
 import '../App.css';
 
 class Gallery extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			playingUrl: '',
+			audio: null,
+			playing: false
+		}
+	}
+
 	playAudio(previewUrl) {
 		let audio = new Audio(previewUrl);
 
-		audio.play();
+		if(!this.state.playing) {
+			audio.play();
+			this.setState({
+				playing:true,
+				playingUrl: previewUrl,
+				audio
+			})
+		} else {
+			if (this.state.playingUrl === previewUrl) {
+				this.state.audio.pause();
+				this.setState({
+					playing: false
+				})
+			} else {
+				this.state.audio.pause();
+				audio.play();
+				this.setState({
+					playingUrl: previewUrl,
+					playing: true,
+					audio
+				});
+			}
+		}
 	}
 
 	render() {
