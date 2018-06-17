ed832b64abbc5e62db1fbfdbb8f8d8bdb280dd2a
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-06-17 21:14:15 +0100

    [reminder_pro] - reducer setup

--- reminder_pro/src/index.js
+++ reminder_pro/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
 import App from './App.jsx';
 
+const store = createStore(reducer);
+
 ReactDom.render(
-	<App/>, document.getElementById('root')
+	<Provider store = {store}>
+		<App/>
+	</Provider>, document.getElementById('root')
 );
