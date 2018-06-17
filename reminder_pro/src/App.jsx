266b0318be77cc9d9a9d39817c7d8657800f71ea
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-06-17 10:23:06 +0100

    [reminder_pro] - initial styling

--- reminder_pro/src/App.jsx
+++ reminder_pro/src/App.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 class App extends React.Component {
 	render() {
 		return (
-			<div>Hello</div>
+			<div className="app">
+				<h1 className="app__title">Reminder Pro</h1>
+				<div className="form-inline">
+					<div className="form-group">
+						<input
+							type="text"
+							className="form-control"
+							placeholder={"I have to..."}
+						/>
+						<button
+							className={"btn btn-success"}>
+							Add reminder
+						</button>
+					</div>
+				</div>
+			</div>
 		)
 	};
 }
