097d0cab99560f5adcb6675d6ebc33f67574a4ff
alexk89 <kozhushko.vasyl@gmail.com>
2018-06-21 16:42:09 +0100

    [ecwid_search] - adjusted component

--- ecwid_search/src/components/Categories.jsx
+++ ecwid_search/src/components/Categories.jsx
@@ -1,39 +1,7 @@
 import React from 'react';
-import {token} from '../token';
 
 export class Categories extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            categories: {},
-        }
-    }
-
-    componentDidMount() {
-        this.getCategories();
-    }
-
-    getCategories() {
-        const url = `${token.url}categories?${token.key}`;
-
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
-        })
-            .then(response => response.json())
-            .then(json => {
-                console.log(json);
-                this.setState({
-                    categories: json
-                })
-            })
-    }
-
-    // Pass selected category to parent
     showCategoryItems(category = {}) {
         this.props.updateCategory(category);
     }
@@ -42,9 +10,9 @@ export class Categories extends React.Component {
         return (
             <ul className="categories">
                 {
-                    this.state.categories.items
+                    this.props.categories.items
                         ?
-                        (this.state.categories.items).map((item, key) => {
+                        (this.props.categories.items).map((item, key) => {
                             return (
                                 <li
                                     className={'categories__item'}
