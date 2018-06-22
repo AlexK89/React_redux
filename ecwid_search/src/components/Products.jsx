c8a58991637256ada7ff23f24f7d057b010da0d4
alexk89 <kozhushko.vasyl@gmail.com>
2018-06-22 09:41:25 +0100

    [ecwid_search] - added image to a product component

--- ecwid_search/src/components/Products.jsx
+++ ecwid_search/src/components/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export class Products extends React.Component {
+
     render() {
         return (
             <div className={"products"}>
@@ -8,10 +9,21 @@ export class Products extends React.Component {
                     this.props.products
                     ?
                         this.props.products.map((product, key) => {
+                            const image = (product.galleryImages[0]) ? product.galleryImages[0].url : '';
                             return (
                                 <div key={key} className={"products__product"}>
                                     <p><strong>Product name:</strong></p>
                                     <p>{product.name}</p>
+                                    <div className="products__product__image">
+                                        {
+                                            image
+                                            ?
+                                                <img src={image} alt={product.name}/>
+                                            :
+                                                ''
+                                        }
+
+                                    </div>
                                     <p><strong>Price:</strong> {product.defaultDisplayedPriceFormatted}</p>
                                     <p><strong>Description:</strong></p>
                                     <div dangerouslySetInnerHTML={{ __html: product.description}} />
