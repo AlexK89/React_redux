064e6f8d2457539483c32bb2d12e585b2ffe7dcd
alexk89 <kozhushko.vasyl@gmail.com>
2018-06-25 14:51:00 +0100

    [akeneo_search] - node middleware

---
+++ akeneo/node_server/index.js
@@ -0,0 +1,31 @@
+// npm install express --save
+// npm install sync-request
+
+const express = require('express');
+const request = require('sync-request');
+
+const categories = express();
+
+categories.get('/:destination', (req, res) => {
+    console.log(req.params);
+    // query params
+    // console.log(req.query);
+
+    qStr = Object.keys(req.query).map(function(key) {
+        return key + '=' + req.query[key];
+    }).join('&');
+
+    // serialised query string
+    // console.log(qStr);
+
+    apiResponse = request('GET', `http://34.247.49.79/api/rest/v1/${req.params.destination}?${qStr}`, {
+        headers: {
+            'Authorization': 'Bearer ZDdlNTNjMmIxYmE5OGYwMmU1MTE2MWUwYTIxZDRmMTdmMDBkMjIzZWI2YzEyMGQ2NTNhODlhNTcwYWZjM2M3NQ'
+        }
+    });
+    apiResponse = apiResponse.getBody().toString();
+
+    res.send(apiResponse);
+});
+
+categories.listen(3000, () => console.log('Example app listening on port 3000!'));
