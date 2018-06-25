810ad2d3666a016a167a22b94f49133246c179b4
alexk89 <kozhushko.vasyl@gmail.com>
2018-06-25 14:51:54 +0100

    [akeneo_search] - products tamplate

--- akeneo/src/components/Products.jsx
+++ akeneo/src/components/Products.jsx
@@ -9,24 +9,43 @@ export class Products extends React.Component {
                     this.props.products
                     ?
                         this.props.products.map((product, key) => {
-                            const image = (product.hdThumbnailUrl) ? product.hdThumbnailUrl : '';
+                            const image = (product.values.HeroImage) ? product.values.HeroImage[0].data : '';
                             return (
                                 <div key={key} className={"products__product"}>
                                     <p><strong>Product name:</strong></p>
-                                    <p>{product.name}</p>
+                                    <p>{product.identifier}</p>
                                     <div className="products__product__image">
                                         {
                                             image
                                             ?
-                                                <img src={image} alt={product.name}/>
+                                                <img src={image} alt={product.identifier}/>
                                             :
                                                 ''
                                         }
 
                                     </div>
-                                    <p><strong>Price:</strong> {product.defaultDisplayedPriceFormatted}</p>
+                                    <p>
+                                        <strong>Price: </strong>
+                                        {
+                                            product.values.Price
+                                            ?
+                                                product.values.Price[0].data.map((price, key) => {
+                                                    return (<span key={key}>{price.amount}{price.currency} </span>);
+                                                })
+                                            :
+                                                ""
+                                        }
+
+                                    </p>
                                     <p><strong>Description:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: product.description}} />
+                                    {
+                                        product.values.Description
+                                        ?
+                                            <div dangerouslySetInnerHTML={{ __html: product.values.Description[0].data}} />
+                                        :
+                                            ""
+                                    }
+
 
                                 </div>
                             )
