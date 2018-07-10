88bfda26699c55ef6ae0568c14ad8b9f672db091
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-07-10 08:29:08 +0100

    [components_separation] - statefull component

--- react_lists/theory/src/components/Persons/Person/Person.jsx
+++ react_lists/theory/src/components/Persons/Person/Person.jsx
@@ -6,7 +6,7 @@ export const Person = (props) => {
         <div className={styles.Person}>
             <p>{props.personId}</p>
             <p onClick = {() => props.deletePersonHandler(props.index)}>I am a {props.name}! I am {props.age} years old</p>
-            {/* Rendering nested HTML from parrent component */}
+            {/* Rendering nested HTML from parent component */}
             <p>{props.children}</p>
             <input
                 onChange={(event) => props.switchNameHandler(event.target.value, props.personId)}
--- react_lists/theory/src/components/Persons/Persons.jsx
+++ react_lists/theory/src/components/Persons/Persons.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import {ErrorBoundary} from '../../ErrorBaundary/ErrorBoundary';
 import {Person} from './Person/Person.jsx';
 
-export const Persons = (props) => {
-	return (
-		props.persons.map((person, index) => {
-			return (
-				<ErrorBoundary key={person.id}>
-					<Person
-						personId={person.id}
-						index={index}
-						deletePersonHandler={props.deletePersonHandler}
-						switchNameHandler={props.switchNameHandler}
-						name={person.name}
-						age={person.age}/>
-				</ErrorBoundary>
-			)
-		})
-	)
+export class Persons extends React.Component {
+	render() {
+		return (
+			this.props.persons.map((person, index) => {
+				return (
+					<ErrorBoundary key={person.id}>
+						<Person
+							personId={person.id}
+							index={index}
+							deletePersonHandler={this.props.deletePersonHandler}
+							switchNameHandler={this.props.switchNameHandler}
+							name={person.name}
+							age={person.age}/>
+					</ErrorBoundary>
+				)
+			})
+		)
+	}
 }
