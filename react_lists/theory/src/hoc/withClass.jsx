92315379be75f9693cac9132fe386da8c2fa0aad
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-07-13 11:18:21 +0100

    [components_separation] - higher order component as a classic react component

--- react_lists/theory/src/hoc/withClass.jsx
+++ react_lists/theory/src/hoc/withClass.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const withClass = (WrappedComponent, className) => {
-	return (props) => (
-		<div className={className}>
-			<WrappedComponent {...props}/>
-		</div>
-	)
+	return class extends React.Component {
+		render() {
+			return (
+				<div className={className}>
+					<WrappedComponent {...this.props}/>
+				</div>
+			)
+		}
+	}
+
 };
 
 export default withClass;
