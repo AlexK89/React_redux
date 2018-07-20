0efbe19a9b34a3b9dec10ddddf9e6df2a034babc
alexk89 <kozhushko.vasyl@gmail.com>
2018-07-20 19:47:23 +0100

    [burger_builder] - Burger ingredients state

--- burger_builder/src/components/Burger/Burger.jsx
+++ burger_builder/src/components/Burger/Burger.jsx
@@ -7,7 +7,6 @@ const burger = (props) => {
         <div className={styles.burger}>
             <BurgerIngredient type="bread-top"/>
             <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="salad"/>
             <BurgerIngredient type="meat"/>
             <BurgerIngredient type="bread-bottom"/>
         </div>
