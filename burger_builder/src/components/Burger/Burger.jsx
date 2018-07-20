dfd70c133a21624efb929555964f3def0530e7c0
alexk89 <kozhushko.vasyl@gmail.com>
2018-07-20 20:00:46 +0100

    [burger_builder] - transform object into array

--- burger_builder/src/components/Burger/Burger.jsx
+++ burger_builder/src/components/Burger/Burger.jsx
@@ -3,11 +3,18 @@ import styles from './Burger.scss';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient.jsx';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])].map((_, i) => {
+                return (<BurgerIngredient key={ingKey + i} type={ingKey}/>)
+            })
+        });
+
+    console.log(transformedIngredients);
     return (
         <div className={styles.burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
