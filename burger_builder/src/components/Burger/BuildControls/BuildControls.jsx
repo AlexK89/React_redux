73734d807fe02f57f9877d6c3ccb4b150aa210a3
alexk89 <kozhushko.vasyl@gmail.com>
2018-07-22 11:53:38 +0100

    [burger_builder] - Build burger controls component

---
+++ burger_builder/src/components/Burger/BuildControls/BuildControls.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from 'BuildControls.scss';
+
+const buildControls = (props) => {
+    return (
+        <div className={styles.BuildControls}></div>
+    )
+};
+
+export default buildControls;
