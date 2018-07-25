9cb77abd3332851cabf28af07234a00c73b49403
alexk89 <kozhushko.vasyl@gmail.com>
2018-07-25 12:07:24 +0100

    [burger_builder] - advanced button component

--- burger_builder/src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
+++ burger_builder/src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import styles from './BuildControl.scss';
+import Button from '../../../UI/Button/Button.jsx';
 
 const buildControl = (props) => {
     return (
         <div className={styles.BuildControl}>
             <div className={styles.label}>{props.label}</div>
-            <button
+            <Button
                 disabled = {!props.ingredient}
-                onClick={props.removeIngredient}
-                className={styles.less}>
+                click={props.removeIngredient}
+                btnType={'danger small'}>
                 Less
-            </button>
-            <button
-                onClick={props.addIngredient}
-                className={styles.more}>
+            </Button>
+            <Button
+                click={props.addIngredient}
+                btnType={'success small'}>
                 More
-            </button>
+            </Button>
         </div>
     )
 };
--- burger_builder/src/components/UI/Button/Button.jsx
+++ burger_builder/src/components/UI/Button/Button.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import styles from './Button.scss';
 
 const button = (props) => {
+    const btnClasses = (props.btnType).split(' ').map(btnClass => {
+        return styles[btnClass];
+    });
+    
     return (
         <button
-            className={[styles.btn, styles[props.btnType]].join(' ')}
+            className={[styles.btn, ...btnClasses].join(' ')}
             onClick={props.click}
             disabled={props.disabled}>
             {props.children}
