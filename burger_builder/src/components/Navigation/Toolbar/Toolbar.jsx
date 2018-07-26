bcf671dbfca4a9c957983d143f9a53a6ed040a92
alexk89 <kozhushko.vasyl@gmail.com>
2018-07-26 18:31:47 +0100

    [burger_builder] - logo added

--- burger_builder/src/components/Navigation/Toolbar/Toolbar.jsx
+++ burger_builder/src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Toolbar.scss';
+import Logo from '../../Logo/Logo.jsx';
 
 const toolbar = (props) => {
     return (
         <header className={styles.Toolbar}>
             <div>Menu</div>
-            <div>Logo</div>
+            <Logo />
             <ul className={styles.Toolbar_nav}>List</ul>
         </header>
     )
