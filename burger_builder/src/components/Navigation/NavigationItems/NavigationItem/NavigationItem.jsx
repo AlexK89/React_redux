2baaed5457886b585c24ef561669975960a0a9c3
alexk89 <kozhushko.vasyl@gmail.com>
2018-07-27 08:45:01 +0100

    [burger_builder] - navigation components

---
+++ burger_builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navigationItem = (props) => {
+    return (
+        <li>
+            <a href="#">A link</a>
+        </li>
+    )
+};
+
+export default navigationItem;
---
+++ burger_builder/src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem.jsx';
+import styles from './NavigationItems.scss';
+
+const navigationItems = (props) => {
+    return (
+        <ul>
+
+        </ul>
+    )
+};
+
+export default navigationItems;
