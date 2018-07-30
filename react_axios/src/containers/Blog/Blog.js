6ece9f7ef68ca8bd0661479646d3cae64092b3a7
alexk89 <kozhushko.vasyl@gmail.com>
2018-07-30 09:33:32 +0100

    [react_axios] - axios get request

--- react_axios/src/containers/Blog/Blog.js
+++ react_axios/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,6 +7,21 @@ import NewPost from '../../components/NewPost/NewPost';
 import styles from './Blog.scss';
 
 class Blog extends Component {
+    state = {
+        posts: null
+    };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                // this.setState({posts: response.data})
+                if (response.status === 200) {
+                    console.log('response: ', response.data);
+                }
+
+            });
+    }
+
     render () {
         return (
             <div>
