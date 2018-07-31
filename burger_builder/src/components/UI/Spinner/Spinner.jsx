db933b55e554ffb70376b6f44556b009911e03e7
alexk89 <kozhushko.vasyl@gmail.com>
2018-07-31 14:49:27 +0100

    [burger_builder] - spinner component

---
+++ burger_builder/src/components/UI/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Spinner.scss';
+
+const spinner = () => {
+    return (
+        <div className={styles.Loader}>Loading...</div>
+    )
+};
+
+export default spinner;
