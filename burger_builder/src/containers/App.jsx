656b20de819acbe44df84f7bcdb69a38ad3cfaaf
alexk89 <kozhushko.vasyl@gmail.com>
2018-08-06 09:48:57 +0100

    [burger_builder] - routing for pages

--- burger_builder/src/containers/App.jsx
+++ burger_builder/src/containers/App.jsx
@@ -1,16 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import styles from './App.scss';
 import Layout from './Layout/Layout.jsx';
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder.jsx';
 import Checkout from '../containers/Checkout/Checkout.jsx';
 
-class App extends Component {
+class App extends React.Component {
     render() {
         return (
             <div className={styles.App}>
                 <Layout>
-                    <BurgerBuilder />
-                    <Checkout />
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route path="/" component={BurgerBuilder}/>
+                    </Switch>
+
                 </Layout>
             </div>
         );
