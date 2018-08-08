ab1af89904a952b12db1084e7646aa9cd8866579
alexk89 <kozhushko.vasyl@gmail.com>
2018-08-08 11:19:39 +0100

    [burger_builder] - handling form submission

--- burger_builder/src/containers/Checkout/ContactData/ContactData.jsx
+++ burger_builder/src/containers/Checkout/ContactData/ContactData.jsx
@@ -9,16 +9,16 @@ class ContactData extends React.Component {
         customer: {
             name: '',
             email: '',
-            address: {
-                street: '',
-                postCode: ''
-            }
+            street: '',
+            postcode: ''
         },
         loading: false
     };
 
     saveOrderHandler = (event) => {
         event.preventDefault();
+        console.log('customer: ', this.state.customer);
+
         const order = {
             ingredients: this.props.ingredients,
             totalPrice: this.props.totalPrice,
@@ -44,21 +44,30 @@ class ContactData extends React.Component {
             });
     };
 
+    submitFormHandler = (event) => {
+        const newState = JSON.parse(JSON.stringify(this.state));
+        const propertyName = event.target.name;
+        newState['customer'][propertyName] = event.target.value;
 
+        this.setState({
+            ...newState
+        })
+    };
 
     render() {
-        console.log("contact data", this.state.customer);
         let form = (this.state.loading) ?
             <Spinner/> :
             <form>
                 <div className={styles.form__name}>
                     <label htmlFor="name">Name</label>
                     <input type="text" id='name' name="name"
+                           onChange={(event) => this.submitFormHandler(event)}
                            placeholder={"Name"}/>
                 </div>
                 <div className={styles.form__email}>
                     <label htmlFor="email">Email</label>
                     <input type="email" id='email' name="email"
+                           onChange={(event) => this.submitFormHandler(event)}
                            placeholder={"Email"}/>
                 </div>
                 <p>Address</p>
@@ -66,11 +75,13 @@ class ContactData extends React.Component {
                     <div className={styles.form__address__street}>
                         <label htmlFor="street">Street</label>
                         <input type="text" id='street' name="street"
+                               onChange={(event) => this.submitFormHandler(event)}
                                placeholder={"Street"}/>
                     </div>
                     <div className={styles.form__address__postcode}>
                         <label htmlFor="postcode">Postcode</label>
                         <input type="text" id="postcode" name="postcode"
+                               onChange={(event) => this.submitFormHandler(event)}
                                placeholder={"Post code"}/>
                     </div>
                 </div>
