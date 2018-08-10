ec973482bf956e2e908d1d6b050c7cd04a8a2fac
alexk89 <kozhushko.vasyl@gmail.com>
2018-08-10 11:50:38 +0100

    [redux] - change state with redux

--- redux/src/containers/Counter/Counter.js
+++ redux/src/containers/Counter/Counter.js
@@ -5,9 +5,6 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
 
     counterChangedHandler = ( action, value ) => {
         switch ( action ) {
@@ -30,8 +27,8 @@ class Counter extends Component {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
                 <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
             </div>
@@ -45,4 +42,11 @@ const mapStateToProps = state => {
   }
 };
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INC_COUNTER'}),
+        onDecrementCounter: () => dispatch({type: 'DEC_COUNTER'}),
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- redux/src/store/reducer.js
+++ redux/src/store/reducer.js
@@ -3,7 +3,25 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  return state;
+    switch (action.type) {
+        case 'INC_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+        case 'ADD_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            };
+        case 'DEC_COUNTER':
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer
