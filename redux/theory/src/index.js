be7aae90e41d21207b27e669541a534e9af28cd3
alexk89 <kozhushko.vasyl@gmail.com>
2018-08-19 11:27:50 +0100

    [react_redux] - dummy middleware

--- redux/theory/src/index.js
+++ redux/theory/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import counterReducer from './store/reducers/counter';
 import resultsReducer from './store/reducers/results';
@@ -8,11 +8,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Async Redux Middleware
+const logger = store => {
+    return next => {
+        return action => {
+            console.log(`[Middleware] Dispatching: ${action}`);
+            const result = next(action);
+            console.log(`[Middleware] Next state: ${store.getState()}`);
+            return result;
+        };
+    }
+};
+
 const rootReducer = combineReducers({
     globalCounter: counterReducer,
     globalResult: resultsReducer
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
